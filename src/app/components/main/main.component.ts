fc187baa0bff9322c26dd8cfcafe6da457021814
BernardoMB <bmondragonbrozon@gmail.com>
2020-06-15 01:10:18 -0500

    Minor css fixes

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -17,4 +17,14 @@ export class MainComponent implements OnInit {
     window.scrollTo(0, 0);
   }
 
+  /* scrollToTop() {
+    (function smoothscroll() {
+      const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - (currentScroll / 8));
+      }
+    })();
+  } */
+
 }
